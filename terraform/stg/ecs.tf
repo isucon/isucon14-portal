ca6f2def36a8946a397668fbdca7113503591599
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2022-05-26 16:38:36 +0900

    stg ECS container insights + execute-command logging

--- terraform/stg/ecs.tf
+++ terraform/stg/ecs.tf
@@ -1,5 +1,22 @@
 resource "aws_ecs_cluster" "main" {
   name = "${local.env}-${local.project}"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+
+  configuration {
+    execute_command_configuration {
+      kms_key_id = aws_kms_key.main.arn
+      logging    = "OVERRIDE"
+
+      log_configuration {
+        cloud_watch_encryption_enabled = true
+        cloud_watch_log_group_name     = aws_cloudwatch_log_group.ecs["execute-command"].name
+      }
+    }
+  }
 }
 
 resource "aws_ecr_repository" "app" {
--- terraform/stg/logs.tf
+++ terraform/stg/logs.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "ecs" {
-  for_each          = toset(["app", "nginx"])
+  for_each          = toset(["app", "nginx", "execute-command"])
   name              = "/ecs/${local.env}/${local.project}/${each.value}"
   retention_in_days = 30
 }
