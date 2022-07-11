4c4622027dfac2ec3c1ebefa0c27b6f1b23c72ff
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2022-07-11 17:23:01 +0900

    fix run-dev

--- supervisor/run-dev.sh
+++ supervisor/run-dev.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
-go build -o /tmp/isuxportal-reporter-testcli ../bench-tool.go/testcli
-export ISUXPORTAL_SUPERVISOR_INSTANCE_NAME=localhost
+pushd ../bench-tool.go/testcli
+go build -o /tmp/isuxportal-reporter-testcli
+popd
+export ISUXPORTAL_SUPERVISOR_INSTANCE_NAME=dummy
 export ISUXPORTAL_SUPERVISOR_ENDPOINT_URL=http://localhost:3000
-export ISUXPORTAL_SUPERVISOR_TOKEN=token
+export ISUXPORTAL_SUPERVISOR_TOKEN=dummy
+export ISUXBENCH_REPORT_FD=3
 export RUST_LOG=${RUST_LOG:-"info,isuxportal_supervisor=trace"}
 exec cargo run --bin isuxportal-supervisor /tmp/isuxportal-reporter-testcli "$@"
