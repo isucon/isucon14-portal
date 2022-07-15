86a74d4575ee63a34cd982714ede69abe9d32ff0
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2022-07-15 10:39:06 +0900

    予選ベンチマーカー用ECSクラスタ、ECR

--- terraform/stg/ecs.tf
+++ terraform/stg/ecs.tf
@@ -28,3 +28,29 @@ resource "aws_ecr_repository" "nginx" {
   name                 = "${local.env}/${local.project}/nginx"
   image_tag_mutability = "MUTABLE"
 }
+
+resource "aws_ecs_cluster" "qualify-benchmarker" {
+  name = "${local.env}-qualify-benchmarker"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+
+  configuration {
+    execute_command_configuration {
+      kms_key_id = aws_kms_key.main.arn
+      logging    = "OVERRIDE"
+
+      log_configuration {
+        cloud_watch_encryption_enabled = false
+        cloud_watch_log_group_name     = aws_cloudwatch_log_group.ecs["execute-command"].name
+      }
+    }
+  }
+}
+
+resource "aws_ecr_repository" "qualify-benchmarker" {
+  name                 = "${local.env}/qualify/benchmarker"
+  image_tag_mutability = "MUTABLE"
+}
