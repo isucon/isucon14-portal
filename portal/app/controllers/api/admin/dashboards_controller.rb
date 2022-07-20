d441f012142d954ef275edac42b3502d9c979460
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2022-07-20 22:50:06 +0900

    fix restore leaderboard cache

--- portal/app/controllers/api/admin/dashboards_controller.rb
+++ portal/app/controllers/api/admin/dashboards_controller.rb
@@ -8,7 +8,8 @@ class Api::Admin::DashboardsController < Api::Admin::ApplicationController
 
     final = Rails.application.config.x.contest.final ? "final" : "qualify"
     clar_count = Clarification.unanswered.requested.count
-    _cache_sum, cached_lb = Rails.cache.read("leaderboard-v2:#{final}:admin")&.yield_self { |_| Isuxportal::Proto::Resources::Leaderboard.decode(_) }
+    cached = Rails.cache.read("leaderboard-v2:#{final}:admin")
+    cached_lb = cached && Isuxportal::Proto::Resources::Leaderboard.decode(cached[1])
     render protobuf: Isuxportal::Proto::Services::Admin::DashboardResponse.new(
       leaderboard: cached_lb || Contest.leaderboard(admin: true, team: nil, history: false),
       unanswered_clarification_count: clar_count,
