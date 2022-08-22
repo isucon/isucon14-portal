6f013b0d513fd097d9cf9261754c958d988b58ff
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2022-08-22 16:27:44 +0900

    本選チームはenvcheckが終わっていることにする

--- portal/app/controllers/api/registration/sessions_controller.rb
+++ portal/app/controllers/api/registration/sessions_controller.rb
@@ -49,7 +49,7 @@ class Api::Registration::SessionsController < Api::Registration::ApplicationCont
       discord_avatar_url: discord_login&.fetch('avatar_url'),
       member_invite_url: @team && registration_url(team_id: @team.id, invite_token: @team.invite_token), # TODO:
       discord_server_id: status == Isuxportal::Proto::Services::Registration::GetRegistrationSessionResponse::Status::JOINED ? Rails.application.config.x.discord.server_id : "",
-      env_check_done: EnvCheck.of_team(current_team).test_ssh_passed.exists?,
+      env_check_done: current_team.promoted? || EnvCheck.of_team(current_team).test_ssh_passed.exists?,
       coupon: coupon&.to_pb
     )
   end
