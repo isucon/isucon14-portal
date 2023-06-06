af971f54535aed1beab43cd9276c6b3f0e5f3cc2
fujiwara <fujiwara.shunichiro@gmail.com>
2023-06-06 09:34:25 +0900

    commitしてなかった

--- supervisor/run-dev.sh
+++ supervisor/run-dev.sh
@@ -3,8 +3,10 @@ pushd ../bench-tool.go/testcli
 go build -o /tmp/isuxportal-reporter-testcli
 popd
 export ISUXPORTAL_SUPERVISOR_INSTANCE_NAME=dummy
-export ISUXPORTAL_SUPERVISOR_ENDPOINT_URL=http://localhost:3000
+export ISUXPORTAL_SUPERVISOR_TEAM_ID=13
+export ISUXPORTAL_SUPERVISOR_INTERVAL_AFTER_EMPTY_RECEIVE=5
+export ISUXPORTAL_SUPERVISOR_ENDPOINT_URL=https://portal.xii.isucon.dev
 export ISUXPORTAL_SUPERVISOR_TOKEN=dummy
-export ISUXBENCH_REPORT_FD=3
 export RUST_LOG=${RUST_LOG:-"info,isuxportal_supervisor=trace"}
-exec cargo run --bin isuxportal-supervisor /tmp/isuxportal-reporter-testcli "$@"
+# exec cargo run --bin isuxportal-supervisor /tmp/isuxportal-reporter-testcli "$@"
+exec cargo run --bin isuxportal-supervisor "$@"
