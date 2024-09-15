32ba2fed4cfd9feca710a05cfe97e9a85a2082d0
hijiki51 <hibiki0719euph@gmail.com>
2024-09-15 14:50:33 +0900

    feat: 🎸 規約・レギュレーションURLの設定

--- portal/config/environments/test.rb
+++ portal/config/environments/test.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
-  config.x.terms_url = ENV.fetch('ISUXPORTAL_TERMS_URL', 'https://isucon.net/archives/54800315.html')
-  config.x.rules_url = ENV.fetch('ISUXPORTAL_RULES_URL', 'https://isucon.net/archives/54753430.html')
+  config.x.terms_url = ENV.fetch('ISUXPORTAL_TERMS_URL', 'https://isucon.net/archives/58657108.html')
+  config.x.rules_url = ENV.fetch('ISUXPORTAL_RULES_URL', 'https://isucon.net/archives/58657116.html')
   config.x.docs_url = ENV.fetch('ISUXPORTAL_DOCS_URL', 'https://gist.github.com/progfay/25edb2a9ede4ca478cb3e2422f1f12f6')
 end
