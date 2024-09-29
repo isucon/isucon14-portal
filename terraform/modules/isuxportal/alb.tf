67b2b3e05ebffc240880ed201e03ee6ec5b8b706
hijiki51 <hibiki0719euph@gmail.com>
2024-09-29 22:05:13 +0900

    Envcheckのみ認証を外す

--- terraform/modules/isuxportal/alb.tf
+++ terraform/modules/isuxportal/alb.tf
@@ -75,6 +75,36 @@ resource "aws_lb_target_group" "app" {
   }
 }
 
+
+
+resource "aws_lb_listener_rule" "app_without_auth" {
+  count        = var.enable_auth ? 1 : 0
+  listener_arn = aws_lb_listener.https.arn
+  priority     = 9
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+
+  condition {
+    host_header {
+      values = [
+        aws_route53_record.portal.name,
+      ]
+    }
+  }
+  condition {
+    path_pattern {
+      values = [
+        "/api/env_check_info",
+        "/api/env_checks",
+      ]
+    }
+  }
+}
+
+
 resource "aws_lb_listener_rule" "app" {
   listener_arn = aws_lb_listener.https.arn
   priority     = 10
@@ -108,3 +138,4 @@ resource "aws_lb_listener_rule" "app" {
     }
   }
 }
+
