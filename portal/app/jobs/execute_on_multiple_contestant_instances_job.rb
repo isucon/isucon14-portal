f615eed7bf47907975357f69d094cc0d0081697e
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-11-16 21:39:24 +0900

    feat: 実行時にテーブルに情報を挿入する

--- portal/app/jobs/execute_on_multiple_contestant_instances_job.rb
+++ portal/app/jobs/execute_on_multiple_contestant_instances_job.rb
@@ -1,8 +1,19 @@
 class ExecuteOnMultipleContestantInstancesJob < ApplicationJob
   def perform(team_ids, command)
-    ContestantInstance.where(team_id: team_ids).order(team_id: :asc).each do |ci|
-      Rails.logger.info "ExecuteOnMultipleContestantInstancesJob: team_id=#{ci.team_id} instance_ip=#{ci.public_ipv4_address}"
-      ExecuteOnContestantInstanceJob.perform_now(ci.team_id, ci.public_ipv4_address, command)
+    request = InstanceCommandExecuteRequest.create!(command: command)
+
+    contestant_instances = ContestantInstance.where(team_id: team_ids).order(team_id: :asc)
+    InstanceCommandExecuteRequestResult.insert_all!(
+      contestant_instances.map do |ci|
+        {
+          instance_command_execute_request_id: request.id,
+          contestant_instance_id: ci.id,
+        }
+      end
+    )
+    InstanceCommandExecuteRequestResult.includes(:contestant_instance).where(instance_command_execute_request_id: request.id).each do |result|
+      Rails.logger.info "ExecuteOnMultipleContestantInstancesJob: team_id=#{result.contestant_instance.team_id} instance_ip=#{result.contestant_instance.public_ipv4_address} command=#{command}"
+      ExecuteOnContestantInstanceJob.perform_now(result.id)
     end
   end
 end
