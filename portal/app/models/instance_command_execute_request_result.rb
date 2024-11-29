d0745896d20104582879ffa9031c36bb0a9efd76
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-11-30 02:14:02 +0900

    fix: don't use composite primary index

--- portal/app/models/instance_command_execute_request_result.rb
+++ portal/app/models/instance_command_execute_request_result.rb
@@ -6,8 +6,8 @@ class InstanceCommandExecuteRequestResult < ApplicationRecord
 
   def to_pb
     Isuxportal::Proto::Resources::InstanceCommandExecuteRequestResult.new(
+      id: id,
       target: contestant_instance.to_pb,
-      output: output,
       exit_code: exit_code,
       finished_at: finished_at.to_time,
     )
--- portal/db/migrate/20241116113729_create_instance_command_execute_request_results.rb
+++ portal/db/migrate/20241116113729_create_instance_command_execute_request_results.rb
@@ -1,6 +1,6 @@
 class CreateInstanceCommandExecuteRequestResults < ActiveRecord::Migration[6.1]
   def change
-    create_table :instance_command_execute_request_results, primary_key: [:instance_command_execute_request_id, :contestant_instance_id] do |t|
+    create_table :instance_command_execute_request_results do |t|
       t.integer :instance_command_execute_request_id, null: false
       t.integer :contestant_instance_id, null: false
       t.text :output
