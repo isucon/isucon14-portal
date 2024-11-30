cf1dcdf526ec2af86a5c6790c3dc5f3ddaa369a8
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-11-30 16:11:28 +0900

    fix: instance command execute result total_finished_count where

--- portal/app/models/instance_command_execute_request.rb
+++ portal/app/models/instance_command_execute_request.rb
@@ -5,13 +5,15 @@ class InstanceCommandExecuteRequest < ApplicationRecord
 
   has_many :instance_command_execute_request_results, dependent: :destroy
 
+  scope :finished, -> { where.not(finished_at: nil) }
+
   def to_pb
     Isuxportal::Proto::Resources::InstanceCommandExecuteRequest.new(
       id: id,
       command: command,
       created_at: created_at.to_time,
       total_target_count: instance_command_execute_request_results.count,
-      total_finished_count: instance_command_execute_request_results.where.not(finished_at:).count,
+      total_finished_count: instance_command_execute_request_results.finished.count,
     )
   end
 end
