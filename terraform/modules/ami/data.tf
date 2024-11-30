8b2fd80c360e73af6619c6d9dd01c1017a5b7827
hijiki51 <hibiki0719euph@gmail.com>
2024-11-30 13:55:58 +0900

    競技用AMIを更新

--- terraform/modules/ami/data.tf
+++ terraform/modules/ami/data.tf
@@ -1,17 +1,18 @@
 data "aws_ami" "envcheck" {
   most_recent = true
   owners      = var.ami_account_ids
+  # https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-images.html#:~:text=is%20UnauthorizedOperation%20.-,%2D%2Dfilters,-(list)
   filter {
     name   = "name"
     values = ["${var.isuconx}-envcheck-*"]
   }
 }
 
-# data "aws_ami" "qualify" {
-#   most_recent = true
-#   owners      = ["self"]
-#   filter {
-#     name   = "tag:Family"
-#     values = ["${var.isuconx}-qualify"]
-#   }
-# }
+data "aws_ami" "qualify" {
+  most_recent = true
+  owners      = var.ami_account_ids
+  filter {
+    name   = "image-id"
+    values = ["ami-0540cf8b82b985755"]
+  }
+}
